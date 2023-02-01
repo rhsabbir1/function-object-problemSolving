e76795986c6fc4ce65b29d8e0798e26dbe9350d4
rhsabbir1 <rhsabbir090@gmai.com>
2023-02-01 15:03:10 +0600

    hot to declear a function

--- apps.js
+++ apps.js
@@ -1,3 +1,4 @@
 // out_line 
 // 1. loop 
 // 2. Object 
+// 3.functio
---
+++ function.js
@@ -0,0 +1,19 @@
+// function
+// how to declear a function
+
+/* 
+function functionName( pramitter){
+    statement or function body 
+    return
+}
+
+coll the function 
+functionName()  
+*/
+
+function rahim(){
+    // console.log("Rahim")
+}
+
+rahim()
+
